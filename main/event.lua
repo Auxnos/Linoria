689ae9fae105392194bc2679f953154fef137c6e
01234567890 <71796281+Auxnos@users.noreply.github.com>
2022-12-04 13:26:09 -0800

    Create event.lua

---
+++ main/event.lua
@@ -0,0 +1,14 @@
+--[[
+Camera CFrame: -1591.02783, 408.9104, -665.85022, 0.379570127, 0.227087453, -0.896860003, 0, 0.969407618, 0.245456696, 0.925163031, -0.0931680277, 0.367958128
+Char CFrame: -1573.3573, 404.074249, -673.099976, 1, 0, 0, 0, 1, 0, 0, 0, 1
+]]
+
+return {
+CFrame = CFrame.new(-1573.3573, 404.074249, -673.099976, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+CameraCFrame = CFrame.new(-1591.02783, 408.9104, -665.85022, 0.379570127, 0.227087453, -0.896860003, 0, 0.969407618, 0.245456696, 0.925163031, -0.0931680277, 0.367958128),
+Event = function()
+firecd(workspace:FindFirstChild("[Snowball Launcher", true):FindFirstChild("ClickDetector", true),0)
+firecd(workspace:FindFirstChild("[Snowball Launcher", true):FindFirstChild("ClickDetector", true),1)
+end,
+EventItemExists = true
+}
